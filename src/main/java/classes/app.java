1feafc9177f36fd877728f1e15a9b2bf21eee89f
Thiago Aguiar <thiagoabaguiar@gmail.com>
2023-09-25 17:30:03 -0300

    Add files via upload

--- src/main/java/classes/app.java
+++ src/main/java/classes/app.java
@@ -24,13 +24,21 @@ public class app {
         palavras.add("Framework");
         palavras.add("DML");
         
+        // Ordem alfabética (default do método sort)
         Collections.sort(palavras);
         System.out.println(palavras);
-        
+
+        // Criando um comparador customizado, baseado no tamanho de cada String
         Collections.sort(palavras, comparadorPorTamanho);        
         System.out.println(palavras);
         
+        // Simplificando o código, usando o comparador customizado
         palavras.sort(comparadorPorTamanho);
+        System.out.println(palavras);
+        
+        // Simplificando ainda mais o código, usando o método compare existente
+        // dentro da classe Integer + Lambda. Sem necessidade de criar comparador customizado        
+        palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));        
         System.out.println(palavras);
         
     }
